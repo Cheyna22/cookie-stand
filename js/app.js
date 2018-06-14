6e8e9b798e78a79479a0c972e064ea23e2f8d359
Cheyna Humphries <humphries.c92@gmail.com>
2018-06-14 10:29:46 -0700

    added a few things in html/js

--- js/app.js
+++ js/app.js
@@ -23,9 +23,14 @@ function Stores(storeName, minCustomers, maxCustomers, avgCookiesPerSale) {
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookiesPerSale = avgCookiesPerSale;
+  this.dailyTotal = dailyTotal;
   storeLocations.push(this);
 }
 
+// creating header
+Stores.makeHeader = function() {
+  var headerEl = document.createElement('hr')
+}
 
 // creating table
 Stores.prototype.renderRow = function() {
@@ -53,7 +58,7 @@ Stores.prototype.renderRow = function() {
   storeTable.appendChild(trElement);
 };
 
-// creating instances of 
+// creating instances of
 var pike = new Stores('Pike Store', 23, 65, 6.3);
 
 
@@ -208,7 +213,7 @@ var capitolHillStore = {
       // give it content
       var capitolHillCookies = this.averagePerHour();
       capitolHillLiEl.textContent = hoursOperation[i] + ': ' + capitolHillCookies + ' cookies';
-      capitolHillTotal += capitolHillCookies; 
+      capitolHillTotal += capitolHillCookies;
       // append to the DOM -> parentElement.appendChild(childElement);
       capitolHillUlEl.appendChild(capitolHillLiEl);
     }
@@ -246,7 +251,7 @@ var alkiStore = {
       // give it content
       var alkiCookies = this.averagePerHour();
       alkiLiEl.textContent = hoursOperation[i] + ': ' + alkiCookies + ' cookies';
-      alkiTotal += alkiCookies; 
+      alkiTotal += alkiCookies;
       // append to the DOM -> parentElement.appendChild(childElement);
       alkiUlEl.appendChild(alkiLiEl);
     }
@@ -264,7 +269,7 @@ seattleCenterStore.makeList();
 capitolHillStore.makeList();
 alkiStore.makeList();
 
-
+Stores.renderRow();
 // Method: a function is a property of an object and must be invoked using do notation, such as firstAndPike.randomCustPerHour()
 
 // This is how to define a function that is NOT a method - function randomCustPerHour() {}
